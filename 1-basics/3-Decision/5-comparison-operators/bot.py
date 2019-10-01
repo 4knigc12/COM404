12bacd959c2dbf077c6e9a2d0f8f0d896fcfe2cf
4knigc12 <4knigc12@solent.ac.uk>
2019-10-01 10:31:47 +0000

    Added code to demonstrate the use of comparison operators.

---
+++ 1-basics/3-Decision/5-comparison-operators/bot.py
@@ -0,0 +1,11 @@
+#understand comparison operators by identify the smallest of two nmbers
+num1= input("please enter first number ")
+print("your first number is " +num1)
+num2= input("Please enter second number ")
+print("Your second number is " + num2)
+if(num1 > num2):
+    print("second number is the smallest")
+if(num1 < num2):
+    print("First number is smallest")
+if(num1 == num2):
+    print("both number are the same")
