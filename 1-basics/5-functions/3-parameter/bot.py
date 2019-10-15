48faeda7ab2ae557041ce21334a3c29429096ad7
4knigc12 <4knigc12@solent.ac.uk>
2019-10-15 10:20:44 +0000

    Added code to demonstrate the use of a user-defined function with a loop.

--- 1-basics/5-functions/3-parameter/bot.py
+++ 1-basics/5-functions/3-parameter/bot.py
@@ -1,13 +1,17 @@
 def escape_by(plan):
     if plan =="jumping over":
         print("We cannot escape that way! The boulder is too big!")
+        return escape_by
     if plan =="running around":
         print("We cannot escape that way! The boulder is moving too fast!")
+        return escape_by
     if plan == "going deeper":
         print("That might just work! Let's go deeper")
+        return escape_by
     else:
         print("We cannot escape that way! The boulder is in the way")
 
 escape_by("jumping over")
 escape_by("running around")
 escape_by("going deeper")
+print()
---
+++ 1-basics/5-functions/4-loop/bot.py
@@ -0,0 +1,19 @@
+def cross_bridge(steps):
+    if steps >5:
+        print("The bridge is collapsing!")
+    for count in range(steps, 0, -1):
+            print("Crossed Steps")
+    if steps<=5:
+        print("Crossed steps")
+    for count in range (steps, 0, -1):
+        print ("Crossed Steps")
+    else:
+        print("We must keep going")    
+ 
+cross_bridge(2)
+cross_bridge(6)
+
+        
+
+
+
