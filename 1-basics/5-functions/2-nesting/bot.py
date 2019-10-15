a332df0753cad953144766faa740611dc7fdca91
4knigc12 <4knigc12@solent.ac.uk>
2019-10-15 08:53:41 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision

---
+++ 1-basics/5-functions/2-nesting/bot.py
@@ -0,0 +1,10 @@
+# Nesting fuctions
+def identify ():
+    print("What do you see?")
+    identify=input()
+    if identify =="A large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine")
+
+identify()
